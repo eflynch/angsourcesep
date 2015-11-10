b99cb40e39e98406e17be80cc7835c25cf83a24b
Evan Lynch <evan.f.lynch@gmail.com>
2015-11-10 01:01:20 -0500

    Added a little more stuff

--- run.py
+++ run.py
@@ -2,8 +2,7 @@ import numpy as np
 import dspy
 import matplotlib.pyplot as plt
 
-# Additions to DSPY
-
+# Additions to DSPY for Mixing Tracks
 class Mixer(dspy.BundleGenerator):
     def __init__(self, generators, mixes):
         self._mixes = mixes
@@ -15,23 +14,15 @@ class Mixer(dspy.BundleGenerator):
 
         for m, g in zip(self._mixes, self.generators):
             data, cf = g.generate(frame_count)
-            output[0::2] += m * data
-            output[1::2] += (1-m) * data
+            # Enfore mono sources
+            buf = dspy.lib.rechannel(data, g.num_channels, 1)
+            # Mix sources
+            output[0::2] += m * buf
+            output[1::2] += (1-m) * buf
 
         return output
 
-class Monofy(dspy.WrapperGenerator):
-    def __init__(self, generator):
-        dspy.WrapperGenerator.__init__(self, generator)
-        self.num_channels = 1
-
-    def _generate(self, frame_count):
-        buf, cf = self.generator.generate(frame_count)
-        return dspy.lib.rechannel(buf, self.generator.num_channels, 1)
-
-
-# Known pan source separation for 2 sources
-
+# Mix signals with pan params a and b
 def ABtoLR(sig, a, b):
     A = sig[0::2]
     B = sig[1::2]
@@ -42,11 +33,13 @@ def ABtoLR(sig, a, b):
     output = np.zeros(len(sig))
     output[0::2] = L
     output[1::2] = R
+
     return output
 
+# Separate signals with pan params a and b
 def LRtoAB(sig, a, b):
     if a == b:
-        raise Exception("Matrix is singular for a==b")
+        raise Exception("Matrix is singular for a==b!!")
     L = sig[0::2]
     R = sig[1::2]
 
@@ -60,7 +53,8 @@ def LRtoAB(sig, a, b):
 
     return output
 
-def separation(sig, num_bins=100, show_plt=False):
+# Spectral Separation Metric
+def spec_separation(sig, num_bins=100, show_plt=False):
     A = sig[0::2]
     B = sig[1::2]
 
@@ -90,40 +84,48 @@ def separation(sig, num_bins=100, show_plt=False):
 
     # return np.arccos(np.dot(binsA, binsB))
 
-def separate(lr, n=20):
+def separate(lr, metric, n=20):
     sep = np.zeros((n,n))
     for i in xrange(n):
         for j in xrange(n):
             if (i == j):
                 continue
-            a = i / float(n)
-            b = j / float(n)
+            b = i / float(n)
+            a = j / float(n)
             ab = LRtoAB(lr, a, b)
-            sep[i,j] = separation(ab, 300)
+            sep[i,j] = metric(ab)
+
 
     amax = np.unravel_index(sep.argmax(), sep.shape)
 
-    return sep, (amax[0]/float(n), amax[1]/float(n))
+    return (round(amax[0]/float(n),2), round(amax[1]/float(n),2)), sep
 
+def plot_separation(sep_graph):
+    sep_graph = np.flipud(sep_graph)
+    fig, ax = plt.subplots()
+    ax.set_title('Separation')
+    cax = ax.imshow(sep_graph, extent=[0, 1, 0, 1], interpolation='nearest')
+    fig.colorbar(cax)
+    ax.set_xlabel('alpha')
+    ax.set_ylabel('beta')
+    plt.savefig('output.png')
 
 
 if __name__=="__main__":
     alpha = 0.8
     beta = 0.2
+    n = 20
 
     # Mix Sources
     violinSampler = dspy.Sampler("./violin.wav")
     polishSampler = dspy.Sampler("./polish.wav")
-    A = Monofy(violinSampler.sample(0, 44100*3, True, 1.0))
-    B = Monofy(polishSampler.sample(0, 44100*3, True, 1.0))
+    A = violinSampler.sample(0, 44100*3, True, 1.0)
+    B = polishSampler.sample(0, 44100*3, True, 1.0)
     stereoMix = Mixer([A, B], [alpha, beta])
 
     lr, fc = stereoMix.generate(44100*5)
 
-    sep, amax = separate(lr, 10)
-    print amax
-    plt.figure()
-    plt.imshow(sep, interpolation='nearest')
-    plt.colorbar()
-    plt.show()
+    pan, graph = separate(lr, lambda sig: spec_separation(sig, 300, False), n)
+    print "Most likely panning values expected at:", pan
+    plot_separation(graph)
 
